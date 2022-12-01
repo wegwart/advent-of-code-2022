0a3eb015c080b99653f2bb42b489dfbbbbe4232b
Robert Wegwart <robert.wegwart@gmail.com>
2022-12-01 07:36:02 +0100

    find the top three elves carrying the most calories

--- day1.py
+++ day1.py
@@ -1,4 +1,4 @@
-def puzzle(file_name):
+def get_subtotals(file_name):
     with open(file_name) as f:  
         subtotal = 0
         subtotals = []    
@@ -10,8 +10,21 @@ def puzzle(file_name):
             value = int(line)
             subtotal += value
         subtotals.append(subtotal)
-        print('{} --> {} --> {}'.format(file_name, subtotals, max(subtotals)))
-        return max(subtotals)
+        return subtotals
+    
+def puzzle(file_name):
+    subtotals = get_subtotals(file_name)
+    result = max(subtotals)
+    print('{} --> {} --> {}'.format(file_name, subtotals, result))
+    return result
+
+def puzzle_part2(file_name):
+    subtotals = get_subtotals(file_name)
+    subtotals.sort(reverse=True)
+    result = sum(subtotals[0:3])
+    print('{} --> {} --> {}'.format(file_name, subtotals, result))
+    return result
 
 if __name__ == '__main__':
     assert(puzzle('day1_test.txt') == 24000)
+    assert(puzzle_part2('day1_test.txt') == 45000)
--- main.py
+++ main.py
@@ -2,3 +2,4 @@ import day1
 
 if __name__ == '__main__':
     assert(day1.puzzle('day1_puzzle.txt') == 70764)
+    assert(day1.puzzle_part2('day1_puzzle.txt') == 203905)
