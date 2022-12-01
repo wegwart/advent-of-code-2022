e7015419a29a88469d7aaeb57913a5110fee1c3d
Robert Wegwart <robert.wegwart@gmail.com>
2022-12-01 06:44:07 +0100

    find the elf carrying the most calories

--- day1.py
+++ day1.py
@@ -1,10 +1,17 @@
 def puzzle(file_name):
-    with open(file_name) as f:
-        lines = 0
+    with open(file_name) as f:  
+        subtotal = 0
+        subtotals = []    
         for index, line in enumerate(f):
-            lines += 1
-        print('{} --> {}'.format(file_name, lines))
-        return lines
+            if line == '\n':
+                subtotals.append(subtotal)
+                subtotal = 0
+                continue
+            value = int(line)
+            subtotal += value
+        subtotals.append(subtotal)
+        print('{} --> {} --> {}'.format(file_name, subtotals, max(subtotals)))
+        return max(subtotals)
 
 if __name__ == '__main__':
-    assert(puzzle('day1_test.txt') == 2)
+    assert(puzzle('day1_test.txt') == 24000)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import day1
 
 if __name__ == '__main__':
-    assert(day1.puzzle('day1_puzzle.txt') == 4)
+    assert(day1.puzzle('day1_puzzle.txt') == 70764)
