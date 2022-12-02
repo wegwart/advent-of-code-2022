111fc98a088557891823f2254888e72d662ee6b8
Robert Wegwart <robert.wegwart@gmail.com>
2022-12-02 08:23:58 +0100

    decrypting using ultra top secret strategy guide

--- day2.py
+++ day2.py
@@ -1,13 +1,3 @@
-
-def strategy_lut(a):
-    if a == 'A':
-        b = 'Y'
-    if a == 'B':
-        b = 'X'
-    if a == 'C':
-        b = 'Z'
-    return b
-
 def decode(code):
     if code in ['A', 'X']:
         select = 'Rock'
@@ -16,6 +6,34 @@ def decode(code):
     if code in ['C', 'Z']:
         select = 'Scissors'
     return select
+
+def decode_end(a):
+    if a == 'X':
+        end = 'lose'
+    if a == 'Y':
+        end = 'draw'
+    if a == 'Z':
+        end = 'win'    
+    return end
+
+def get_response(opponent, end):
+    if end == 'lose':
+        if opponent == 'Rock':
+            you = 'Scissors'
+        elif opponent == 'Scissors':
+            you = 'Paper'
+        elif opponent == 'Paper':
+            you = 'Rock'
+    elif end == 'draw':
+        you = opponent
+    elif end == 'win':
+        if opponent == 'Scissors':
+            you = 'Rock'
+        elif opponent == 'Paper':
+            you = 'Scissors'
+        elif opponent == 'Rock':
+            you = 'Paper'        
+    return you
     
 def get_score(a, b):
     if a == 'Rock':
@@ -40,8 +58,26 @@ def get_scores(file_name):
             scores.append(get_score(you, opponent))      
         return scores
     
+def get_scores_part2(file_name):
+    with open(file_name) as f:
+        scores = []
+        for index, line in enumerate(f):
+            selection = line.strip().split(' ')
+            opponent = decode(selection[0])
+            end = decode_end(selection[1])
+            you = get_response(opponent, end)
+            scores.append(get_score(you, opponent))        
+        return scores
+    
 def puzzle(file_name, verbose = False):
     scores = get_scores(file_name)
+    result = sum(scores)
+    if verbose:
+        print('{} --> {} --> {}'.format(file_name, scores, result))
+    return result
+    
+def puzzle_part2(file_name, verbose = False):
+    scores = get_scores_part2(file_name)
     result = sum(scores)
     if verbose:
         print('{} --> {} --> {}'.format(file_name, scores, result))
@@ -50,3 +86,4 @@ def puzzle(file_name, verbose = False):
 if __name__ == '__main__':
     verbose = True
     puzzle('day2_test.txt', verbose)
+    puzzle_part2('day2_test.txt', verbose)
