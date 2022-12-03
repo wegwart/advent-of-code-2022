10d871f1b7a21a5621291c353d313eba73b64433
Robert Wegwart <robert.wegwart@gmail.com>
2022-12-03 14:47:48 +0100

    count assignment pairs where ranges overlap

--- README.md
+++ README.md
@@ -3,4 +3,3 @@
 https://adventofcode.com/
 
 ![https://unsplash.com/s/photos/weihnachten-wegwart](/resources/jonathan-gohner-g7Vu4WjSBA0-unsplash.jpg)
-
--- day4/day4.py
+++ day4/day4.py
@@ -1,6 +1,6 @@
 import pathlib
           
-def get_item_results(file_name):
+def get_item_results(file_name, is_part_two):
     with open(file_name) as f:
         item_results = []
         for index, line in enumerate(f):
@@ -10,14 +10,27 @@ def get_item_results(file_name):
             a = list(range(left[0], left[1]+1))
             b = list(range(right[0], right[1]+1))
             c = list(set(a).intersection(b))
-            if set(a).issubset(c) or set(b).issubset(c):
-                item_results.append(1)
-            else:
-                item_results.append(0)            
+            if is_part_two:
+                if set(a).intersection(c) != set() or set(b).intersection(c) != set():
+                    item_results.append(1)                                    
+                else:
+                    item_results.append(0)        
+            else:        
+                if set(a).issubset(c) or set(b).issubset(c):
+                    item_results.append(1)
+                else:
+                    item_results.append(0)        
         return item_results
        
 def puzzle(file_name, verbose = False):
-    item_results = get_item_results(file_name)
+    item_results = get_item_results(file_name, is_part_two=False)
+    result = sum(item_results)
+    if verbose:
+        print('{} --> {} --> {}'.format(file_name, item_results, result))
+    return result
+
+def puzzle_part2(file_name, verbose = False):
+    item_results = get_item_results(file_name, is_part_two=True)
     result = sum(item_results)
     if verbose:
         print('{} --> {} --> {}'.format(file_name, item_results, result))
@@ -27,10 +40,18 @@ __location__ = pathlib.Path(__file__).parent
 
 def test_puzzle():
     assert(puzzle(__location__ / 'day4_test.txt', verbose=True) ==  2)
-    
+        
 def print_puzzle():
     print(puzzle(__location__ / 'day4_puzzle.txt'))
+    
+def test_puzzle_part2():
+    assert(puzzle_part2(__location__ / 'day4_test.txt', verbose=True) ==  4)
+        
+def print_puzzle_part2():
+    print(puzzle_part2(__location__ / 'day4_puzzle.txt'))    
     
 if __name__ == '__main__':
     test_puzzle()
     print_puzzle()
+    test_puzzle_part2()
+    print_puzzle_part2()    
