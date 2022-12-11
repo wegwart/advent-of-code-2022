2dc4644709c779fe3187cbb8fbf92255c7db7dea
Robert Wegwart <robert.wegwart@gmail.com>
2022-12-11 22:15:00 +0100

    level of monkey business after 20 rounds of stuff-slinging simian shenanigans

---
+++ day11/day11.py
@@ -0,0 +1,75 @@
+import pathlib
+    
+def get_inspected_items(file_name, iterations):
+    with open(file_name) as f:
+        notes = []
+        while True:
+            n0 = int(f.readline().strip().replace('Monkey', '').replace(':', ''))
+            n1 = list(map(int, f.readline().strip().replace('Starting items:','').split(',')))
+            n2, n3 = f.readline().strip().split(' ')[4:]
+            n4 = int(f.readline().strip().split(' ')[3])
+            n5 = int(f.readline().strip().split(' ')[5])
+            n6 = int(f.readline().strip().split(' ')[5])
+            notes.append((n0, n1, n2, n3, n4, n5, n6))
+            if len(f.readline()) < 1:
+                break
+    items = [[] for _ in range(len(notes))]
+    count_inspecting_items = [0] * len(notes)
+    for iteration in range(iterations):
+        for note in notes:
+            monkey = note[0]
+            starting_items = note[1]
+            operator = note[2]
+            operand = note[3]
+            test = note[4]
+            if_true = note[5]
+            if_false = note[6]
+            results = []
+            if iteration == 0:
+                starting_items += items[monkey]
+                items[monkey].clear()        
+            else:
+                starting_items.clear()
+                starting_items += items[monkey]
+                items[monkey].clear()                        
+            for starting_item in starting_items:
+                count_inspecting_items[monkey] += 1
+                worry_level = starting_item
+                if operand == 'old':
+                    operand_value = worry_level
+                else:
+                    operand_value = int(operand)
+                if operator == '+':
+                    worry_level += operand_value
+                elif operator == '*':
+                    worry_level *= operand_value
+                worry_level = int(worry_level / 3)
+                if  worry_level % test == 0:
+                    throw_to = if_true
+                else:
+                    throw_to = if_false
+                results.append((worry_level, throw_to))
+                items[throw_to].append(worry_level)
+            print('{} --> {}'.format(note, results))
+        print('{} --> {}'.format(iteration, items))                
+    return count_inspecting_items
+
+def puzzle(file_name, iterations, verbose = False):
+    inspected_items = get_inspected_items(file_name, iterations)
+    inspected_items.sort()
+    result = inspected_items[-1] * inspected_items[-2]
+    if verbose:
+        print('{} --> {} --> {}'.format(file_name, inspected_items, result))
+    return result
+
+__location__ = pathlib.Path(__file__).parent
+   
+def test_puzzle():
+    assert(puzzle(__location__ / 'day11_test.txt', iterations=20, verbose=True) == 10605) 
+          
+def print_puzzle():
+    print(puzzle(__location__ / 'day11_puzzle.txt', iterations=20))
+              
+if __name__ == '__main__':
+    test_puzzle()
+    print_puzzle()
